6f5a4a1018e1b0384ad6086415801d99678757b8
Ka3y6 <kozubne@gmail.com>
2025-05-30 20:22:27 +0300

    Switch from MySQL to PostgreSQL database configuration

--- backend/logistic_crm/settings.py
+++ backend/logistic_crm/settings.py
@@ -176,16 +176,12 @@ WSGI_APPLICATION = 'logistic_crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'greatlineby_crm',
         'USER': 'greatlineby_root',
         'PASSWORD': 'c64s6KPa!',
         'HOST': 'db',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        }
+        'PORT': '5432',
     }
 }
 
