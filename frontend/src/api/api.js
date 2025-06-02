133e9861940aea98d5ccd8cc86cd8879e7fa312a
Ka3y6 <kozubne@gmail.com>
2025-06-02 16:25:51 +0300

    Обновлен базовый URL API для подключения к серверу

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://185.135.83.113:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
