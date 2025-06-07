a6f680f92751d1498295654f94612d4fc15d8d2b
Ka3y6 <you@example.com>
2025-06-07 14:49:57 +0300

    fix: настройка использования переменных окружения

--- backend/site_requests/apps.py
+++ backend/site_requests/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class RequestsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'requests'
+    name = 'site_requests'
     verbose_name = 'Заявки с сайта' 
