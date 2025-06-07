8dc2c966c79b422db23424f37290797a7f019c45
Ka3y6 <you@example.com>
2025-06-07 15:50:36 +0300

    fix: настройка использования переменных окружения

--- backend/site_requests/models.py
+++ backend/site_requests/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from api.models import User
+from api.models import CustomUser as User
 
 class Request(models.Model):
     STATUS_CHOICES = [
